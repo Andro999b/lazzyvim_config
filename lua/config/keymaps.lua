a25e9637781acbc72c7209463cf54bbc5272a3a7
Andrii Pyvovarov <a.pyvovarov@steel.mil.ua>
2025-01-30 09:31:17 +0200

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- local map = LazyVim.safe_keymap_set
+--
+-- map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+-- map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+-- map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+-- map("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+-- map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+-- map(
+-- 	"n",
+-- 	"<Leader>dd",
+-- 	"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+-- 	{ desc = "Debugger set conditional breakpoint" }
+-- )
+-- map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+-- map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+--
+-- map("n", "<Leader>rt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Rust testables" })
+-- map("n", "<Leader>rd", "<cmd>lua vim.cmd('RustLsp debuggables')<CR>", { desc = "Rust debuggables" })
+-- map("n", "<Leader>ri", "<cmd>lua vim.cmd.RustLsp({ 'renderDiagnostic', 'current'})<CR>", { desc = "Rust Diagnostic" })
+-- map("n", "<Leader>re", "<cmd>lua vim.cmd.RustLsp({ 'explainError', 'current'})<CR>", { desc = "Rust Error" })
+-- map("n", "<Leader>ra", "<cmd>lua vim.cmd.RustLsp({ 'codeAction', 'current'})<CR>", { desc = "Rust Code Action" })
+-- map("n", "<Leader>rs", "<cmd>lua vim.cmd.RustLsp('workspaceSymbol')<CR>", { desc = "Rust Symbols" })
---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.rustaceanvim = {
+  tools = {
+    enable_nextest = false,
+  },
+}
+
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
